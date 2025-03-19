b2f6845e761724e899a5d86146fffe6067cef463
Iheagwaram <tiheagwaram5@gmail.com>
2025-03-19 16:41:32 +0100

    player cards images fixed

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami CF",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/250px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+    },
+    {
+      name: "Kevin De Bruyne",
+      team: "Manchester City",
+      nationality: "Belgium",
+      jerseyNumber: 17,
+      age: 32,
+      imageUrl: "https://assets.goal.com/images/v3/blt3d963cb7a3a2ebb0/GettyImages-1381404937.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://phantom-marca.unidadeditorial.es/dbc144f9ba51fd525ea1c829e42cb96f/resize/828/f/jpg/assets/multimedia/imagenes/2024/02/18/17082948959606.jpg",
+    },
+  ];
+  
+  export default players;
+  
